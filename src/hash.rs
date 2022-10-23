1adbff85a983ce29fe6d56d3c68075a570187884
RoccoDev <hey@rocco.dev>
2022-10-23 18:30:28 +0200

    meta: add license files and package meta

--- src/hash.rs
+++ src/hash.rs
@@ -10,8 +10,8 @@ pub fn murmur3(src: &str) -> u32 {
     for _ in 0..(len as usize >> 2) {
         buf.copy_from_slice(&bytes[start..(start + 4)]);
         hash ^= murmur3_scramble(u32::from_le_bytes(buf));
-        hash = (hash << 13) | (hash >> 19);
-        hash = hash * 5 + 0xe6546b64;
+        hash = (hash.wrapping_shl(13)) | (hash.wrapping_shr(19));
+        hash = hash.wrapping_mul(5).wrapping_add(0xe6546b64);
         start += 4;
     }
 
@@ -23,18 +23,18 @@ pub fn murmur3(src: &str) -> u32 {
 
     hash ^= murmur3_scramble(k);
     hash ^= len;
-    hash ^= hash >> 16;
+    hash ^= hash.wrapping_shr(16);
     hash = hash.wrapping_mul(0x85ebca6b);
-    hash ^= hash >> 13;
+    hash ^= hash.wrapping_shr(13);
     hash = hash.wrapping_mul(0xc2b2ae35);
-    hash ^= hash >> 16;
+    hash ^= hash.wrapping_shr(16);
     hash
 }
 
 #[inline]
 fn murmur3_scramble(mut k: u32) -> u32 {
     k = k.wrapping_mul(0xcc9e2d51);
-    k = (k << 15) | (k >> 17);
+    k = (k.wrapping_shl(15)) | (k.wrapping_shr(17));
     k = k.wrapping_mul(0x1b873593);
     k
 }
